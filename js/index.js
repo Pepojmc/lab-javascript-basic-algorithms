f2d54a164cec0187d9d3d49a393eacb5ba104003
Pepojmc <pepojmc@gmail.com>
2022-07-19 20:52:26 +0200

    Interation 1 Done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 // Iteration 1: Names and Input
 console.log("I'm ready!"); 
 
+let hacker1 = "Joan"
+
+console.log("The driver's name is " + hacker1);
+
+let hacker2 = "Pepo"
+
+console.log("The navigator's name is " + hacker2);
+
 // Iteration 2: Conditionals
 
 
